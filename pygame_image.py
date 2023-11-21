0c9bbbe7a98776433457f6bce2c37224dfd57fd7
c0b22175e4 <c0b22175e4@edu.teu.ac.jp>
2023-11-21 15:32:35 +0900

    練習2

---
+++ pygame_image.py
@@ -0,0 +1,28 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1
+    kk_img = pg.image.load("ex01/fig/3.png") # 練習2
+    pg.transform.flip(kk_img, True, False) # 練習2
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0]) #練習4
+        #screen.blit(kk_img, [300, 200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
