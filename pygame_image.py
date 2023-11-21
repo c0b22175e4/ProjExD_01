0169da33ec4ecb34b893254357ca1c7660b24f45
c0b22175e4 <c0b22175e4@edu.teu.ac.jp>
2023-11-21 18:12:29 +0900

    演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -8,20 +8,35 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20231121/fig/pg_bg.jpg") #練習1
     kk_img = pg.image.load("ex01-20231121/fig/3.png") # 練習2
-    pg.transform.flip(kk_img, True, False) # 練習2
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)] # 練習3
+    kk_img = pg.transform.flip(kk_img, True, False) # 練習2
+    bg_imgs = [bg_img, pg.transform.flip(bg_img, True, False)]
+    kk_imgs = [kk_img,pg.transform.rotozoom(kk_img, 4, 1.0), pg.transform.rotozoom(kk_img, 7, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0)] # 練習3
     tmr = 0
     while True:
+        #a += 1
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        x = tmr%1600
-        screen.blit(bg_img, [-x, 0]) #練習4
-        screen.blit(bg_img, [1600-x, 0]) # 練習6
+        x = tmr%4800
+        screen.blit(bg_imgs[0], [-x, 0]) #練習4
+        screen.blit(bg_imgs[1], [1600-x, 0]) # 練習6
+        screen.blit(bg_imgs[0], [3200-x, 0])
+        screen.blit(bg_imgs[1], [4800-x, 0])
         screen.blit(kk_imgs[tmr%2], [300, 200]) # 練習5
+
+        if tmr % 8 == 0 or tmr % 8 == 7 :
+            screen.blit(kk_imgs[0], [300, 200])
+        elif tmr % 8 == 1 or tmr % 8 == 6:
+            screen.blit(kk_imgs[1], [300, 200])
+        elif tmr % 8 == 2 or tmr % 8 == 5:
+            screen.blit(kk_imgs[2], [300, 200])
+        else:
+            screen.blit(kk_imgs[3], [300, 200])
+
         pg.display.update()
-        tmr += 1        
-        clock.tick(100)
+        tmr += 1
+        #x += 5        
+        clock.tick(60)
 
 
 if __name__ == "__main__":
